3e754c4de1b9bd55c538490ba8893aeb5d46c8ba
Anastasiia <thelisafox@gmail.com>
2024-09-04 15:33:00 +0200

    Customization, Camera and 2D Lighting

---
+++ Assets/Scripts/HairMover.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HairMover : MonoBehaviour
+{
+    public GameObject hair;
+    public string PlayScene;
+    GameObject hairOriginal;
+    Transform HairTransform;
+
+    string sceneName;
+
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(PlayScene, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(hair, SceneManager.GetSceneByName(PlayScene));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+
+    // Update is called once per frame
+    public void NextScene()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.activeSceneChanged += SceneChange;
+
+    }
+
+    private void SceneChange(Scene prevScene, Scene nextScene)
+    {
+
+        Debug.Log(nextScene);
+        hairOriginal = GameObject.FindGameObjectWithTag("Hair");
+        HairTransform = hairOriginal.GetComponent<Transform>();
+        Move(HairTransform);
+        //Scene currentScene = nextScene;
+        //sceneName = currentScene.name;
+
+        //if (sceneName == "PlayScene")
+        //{
+
+        //}
+    }
+
+    public void Move(Transform newParent)
+    {
+
+        this.transform.SetParent(newParent, false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
