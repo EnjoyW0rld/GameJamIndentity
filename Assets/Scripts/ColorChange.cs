ad97fb06afdd5c01bbf4ed9bd71c336c8cff4897
Anastasiia <thelisafox@gmail.com>
2024-09-04 22:46:40 +0200

    customization scene fix
    - fixed buttons not working in cuztomization scene
    - fixed buttons stopping working after a while in cuztomization scene
    - deleted duplicate old code from ColorChange script

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -12,32 +12,10 @@ public class ColorChange : MonoBehaviour
     void Start()
     {
         hair = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+    public void ChangeColor()
+    {
         hair.GetComponent<SpriteRenderer>().color = newcolor;
-    }
-
-    IEnumerator LoadYourAsyncScene()
-    {
-        // Set the current Scene to be able to unload it later
-        Scene currentScene = SceneManager.GetActiveScene();
-
-        // The Application loads the Scene in the background at the same time as the current Scene.
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(PlayScene, LoadSceneMode.Additive);
-
-        // Wait until the last operation fully loads to return anything
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-
-        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
-        SceneManager.MoveGameObjectToScene(hair, SceneManager.GetSceneByName(PlayScene));
-        // Unload the previous Scene
-        SceneManager.UnloadSceneAsync(currentScene);
-    }
-
-    // Update is called once per frame
-    public void NextScene()
-    {
-        StartCoroutine(LoadYourAsyncScene());
     }
 }
